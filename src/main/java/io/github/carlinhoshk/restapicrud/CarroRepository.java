49143ac6270ff75e97321ed568298b42c3174f57
carlos Oliveira <carlosmdohk@gmail.com>
2022-10-25 17:03:52 -0300

    17:02 feito métodos post,get, delet e fim do CRUD

---
+++ src/main/java/io/github/carlinhoshk/restapicrud/CarroRepository.java
@@ -0,0 +1,7 @@
+package io.github.carlinhoshk.restapicrud;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarroRepository extends JpaRepository<Carro, Long> {
+
+}
