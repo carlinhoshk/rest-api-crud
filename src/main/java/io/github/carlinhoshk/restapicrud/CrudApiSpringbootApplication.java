811cb85a51110444111351bb988d3dc7878bb429
carlos Oliveira <carlosmdohk@gmail.com>
2022-10-25 16:30:26 -0300

    Primeiro commit

---
+++ src/main/java/io/github/carlinhoshk/restapicrud/CrudApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.github.carlinhoshk.restapicrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/carlinhoshk/restapicrud/CrudApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.carlinhoshk.restapicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
